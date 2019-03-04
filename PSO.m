bf50232f8eb3a1e844af8624d82699ed546e6ea8
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2019-03-04 11:22:00 -0500

    Add files via upload

--- PSO.m
+++ PSO.m
@@ -6,20 +6,22 @@ classdef PSO < handle
         intertia = 1.1
         c1 = 2
         c2 = 2
+        file_name
         data
         get_data
     end
     methods
-        function obj = PSO(num_particles, data)
+        function obj = PSO(file_name, num_particles, data)
+            obj.file_name = file_name;
             obj.particles = Particle;
             for n = 2:num_particles
                 obj.particles(end + 1) = Particle;
             end
             obj.best_position = obj.particles(1).best_position;
             switch nargin
-                case 1
-                    obj.get_data = false;
                 case 2
+                    obj.get_data = false;
+                case 3
                     obj.data = data;
                     obj.get_data = true;
             end
@@ -33,11 +35,6 @@ classdef PSO < handle
                     figure(1);
             end
             
-%             path(path, 'C:\Program Files\Lumerical\device\api\matlab');
-%             h = appopen('device');
-%             path(path, 'C:\Program Files\Lumerical\mode\api\matlab');
-%             r = appopen('mode');
-
             path(path, 'C:\Program Files\Lumerical\fdtd\api\matlab');
             h = appopen('fdtd');
             
@@ -46,9 +43,7 @@ classdef PSO < handle
             for k = 1:num_iterations
                 for m = 1:length(obj.particles)
                     waitbar(count/(num_iterations*length(obj.particles)));
-%                     code = strcat('load("H:/photonmind-master/Devices/disk_zipper_junc.ldev");',...
-%                         'switchtolayout;');
-                    code = strcat('load("H:/photonmind-master/Devices/grating_coupler_2D_TM.fsp");',...
+                    code = strcat('load("',char(obj.file_name),'");',...
                         'switchtolayout;');
                     appevalscript(h, code);
                     
@@ -61,29 +56,14 @@ classdef PSO < handle
                     
                     code = strcat('run;');
                     appevalscript(h, code);
-                    
-%                     code = strcat('load("H:/photonmind-master/Devices/disk_FDE.lms");',...
-%                         'switchtolayout;',...
-%                         'select("np density");',...
-%                         'set("V_anode_index", 1);',...
-%                         'importdataset("disk_zipper_junc.mat");',...
-%                         'findmodes;',...
-%                         'a = getdata("mode1", "neff");',...
-%                         'loss = imag(a);',...
-%                         'switchtolayout;',...
-%                         'select("np density");',...
-%                         'set("V_anode_index", 3);',...
-%                         'findmodes;',...
-%                         'b = getdata("mode1", "neff");',...
-%                         'del_neff = abs(a - b);',...
-%                         'fom = del_neff/loss;');
 
                     % get FOM
-                    code = strcat('port = getresult("FDTD::ports::port 2", "T");',...
-                        'T = port.T;',...
-                        'T_min = abs(min(T));');
+                    % for now, this will be custom-written
+                    % for example,
+                    code = strcat('port = getresult("FDTD::ports::port 1", "T");',...
+                        'FOM = min(port.T);');
                     appevalscript(h, code);
-                    obj.particles(m).FOM = appgetvar(h, 'T_min')';
+                    obj.particles(m).FOM = appgetvar(h, 'FOM')';
                     
                     % update global bests
                     if obj.particles(m).FOM >= obj.best_FOM
@@ -97,7 +77,7 @@ classdef PSO < handle
                         obj.particles(m).best_position = obj.particles(m).position;
                     end
                     
-                    % add example to dataset
+                    % add example to dataset (if applicable)
                     if obj.get_data == true
                         obj.data.examples(end + 1).features = obj.particles(m).position;
                         obj.data.examples(end).labels = obj.particles(m).FOM;
@@ -106,7 +86,7 @@ classdef PSO < handle
                     % plot data
                     if plot_data == true
                         scatter(obj.particles(m).position(1), obj.particles(m).position(2));
-                        ylim([0.1 0.9]);  xlim([2e-8 2e-7]);
+                        ylim(obj.particles(1).inputs(2).range);  xlim(obj.particles(1).inputs(1).range);
                         hold on;
                     end
                     
@@ -117,6 +97,8 @@ classdef PSO < handle
                     obj.particles(m).position = obj.particles(m).position + obj.particles(m).velocity;
                     
                     % boundary condition
+                    % NEED A SMARTER METHOD
+                    % for now, the particle is moved to the boundary it tries to cross
                     for n = 1:length(obj.particles(m).inputs)
                         if obj.particles(m).position(n) > obj.particles(m).inputs(n).range(2)
                             obj.particles(m).position(n) = obj.particles(m).inputs(n).range(2);
