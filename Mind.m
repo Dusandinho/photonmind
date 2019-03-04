f1506b0ae540ad5132565ed04fa277ba94078522
Dusan <dusan.gostimirovic@gmail.com>
2019-03-04 12:01:03 -0500

    Clean up

--- Mind.m
+++ Mind.m
@@ -30,10 +30,10 @@ classdef Mind < handle
             obj.examples(end).validate = obj.data.examples((round(obj.ratio(1)*end) + 1):round((obj.ratio(1) + obj.ratio(2))*end));
             obj.examples(end).test = obj.data.examples(round(((obj.ratio(1) + obj.ratio(2))*end) + 1):end);
         end
-        
+
         function init_ANN(obj, shape)
             rng('default');
-            
+
             obj.layers = Layer('input', length(obj.data.examples(1).features), 'none');
             for n = 1:length(shape)
                 obj.layers(end + 1) = Layer('hidden', shape(n));
@@ -52,7 +52,7 @@ classdef Mind < handle
                 [1, obj.layers(length(obj.layers)).num_neurons])...
                 /obj.layers(length(obj.layers) - 1).num_neurons;
         end
-        
+
         function train(obj, num_epochs, test_data)
             switch nargin
                 case 2
@@ -60,18 +60,18 @@ classdef Mind < handle
                 case 3
                     plot_test_error = true;
             end
-            
+
             format long;
             v = waitbar(0, 'Training...');
             error_list_train = zeros(1, num_epochs*ceil(obj.sample_size/obj.batch_size));
             error_list_validate = zeros(1, num_epochs*ceil(obj.sample_size/obj.batch_size));
             error_list_test = zeros(1, num_epochs*ceil(obj.sample_size/obj.batch_size));
-            
+
             features = reshape([obj.examples.train(1:obj.sample_size).features],...
                 [length(obj.examples.train(1).features) obj.sample_size])';
             labels = reshape([obj.examples.train(1:obj.sample_size).labels],...
                 [length(obj.examples.train(1).labels) obj.sample_size])';
-            
+
             batches = struct('features', {}, 'labels', {});
             for n = 1:floor(obj.sample_size/obj.batch_size)
                 batches(end + 1).features = features(((n - 1)*obj.batch_size + 1):n*obj.batch_size, :); %#ok
@@ -81,7 +81,7 @@ classdef Mind < handle
                 batches(end + 1).features = features((n*obj.batch_size + 1):obj.sample_size, :);
                 batches(end).labels = labels((n*obj.batch_size + 1):obj.sample_size, :);
             end
-            
+
             for m = 1:num_epochs
                 waitbar(m/num_epochs);
                 for k = 1:length(batches)
@@ -90,21 +90,21 @@ classdef Mind < handle
                     features = features(ix, :);
                     labels = batches(k).labels;
                     labels = labels(ix, :);
-                    
+
                     obj.layers(1).net = obj.scale(features, 'f');
                     obj.layers(1).out = obj.layers(1).net;
                     for n = 2:length(obj.layers)
                         obj.layers(n).feed(obj.layers(n - 1), obj.weights{n - 1}, obj.biases{n - 1});
                     end
-                    
+
                     error = mean(mean(abs((obj.scale(labels, 'l') - obj.layers(end).out))));
-                    
+
                     derr = obj.layers(end).out - obj.scale(labels, 'l');
                     dout = ones(size(obj.layers(end).out));
                     dnet = obj.layers(end - 1).out;
                     obj.layers(end).dw = dnet'*(derr.*dout);
                     obj.layers(end).db = ones(size(features, 1), 1)'*(derr.*dout);
-                    
+
                     for n = (length(obj.layers) - 1):-1:2
                         derr = derr*obj.weights{n}';
                         dout = obj.layers(n).dACT(obj.layers(n).net);
@@ -112,7 +112,7 @@ classdef Mind < handle
                         obj.layers(n).dw = dnet'*(derr.*dout);
                         obj.layers(n).db = ones(size(features, 1), 1)'*(derr.*dout);
                     end
-                    
+
                     obj.optimizer.optimize(obj);
 
                     error_list_train(m*length(batches) + k - 1) = error;
@@ -133,17 +133,17 @@ classdef Mind < handle
             end
             close(v);
         end
-        
+
         function validation_error = validate(obj)
             validation_error = mean(mean(abs(obj.scale(reshape([obj.examples.validate.labels],...
                 [length(obj.examples.validate(1).labels) length(obj.examples.validate)])', 'l')...
                 - obj.infer(reshape([obj.examples.validate.features],...
                 [length(obj.examples.validate(1).features) length(obj.examples.validate)])', false))));
         end
-        
-        
+
+
         function test_error = test(obj, data)
-            labels = reshape([data.examples.labels], [length(data.examples(1).labels) length(data.examples)])'; 
+            labels = reshape([data.examples.labels], [length(data.examples(1).labels) length(data.examples)])';
             predictions = obj.infer(reshape([data.examples.features],...
                 [length(data.examples(1).features) length(data.examples)])', false);
             error = abs(labels - predictions);
@@ -160,7 +160,7 @@ classdef Mind < handle
             obj.layers(1).out = obj.layers(1).net;
             for n = 2:length(obj.layers)
                 obj.layers(n).feed(obj.layers(n - 1), obj.weights{n - 1}, obj.biases{n - 1});
-            end  
+            end
             y = obj.layers(end).out;
             if descale == true, y = obj.descale(y); end
         end
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # photonmind
-Design photonic integrated circuits with artificial neural networks.
+Accelerate the design of photonic integrated circuits (PICs) with machine learning.
+
+## About
+The PhotonMind platform creates compact, flexible models of photonic devices using numerically solved electromagnetic data and deep artificial neural networks (ANNs). This allows for orders-of-magnitude faster calculations for device-level optimizations and system-level simulations.
+
+**PhotonMind is written in MATLAB.**
+
+## License
+This project is licensed under the Apache-2.0 License - see the [LICENSE](LICENSE) file for details
