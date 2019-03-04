abe4ba422c65b410217755a8c25f5826ea0901b6
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2019-03-04 11:43:28 -0500

    Add files via upload

--- Mind.m
+++ Mind.m
@@ -54,6 +54,13 @@ classdef Mind < handle
         end
         
         function train(obj, num_epochs, test_data)
+            switch nargin
+                case 2
+                    plot_test_error = false;
+                case 3
+                    plot_test_error = true;
+            end
+            
             format long;
             v = waitbar(0, 'Training...');
             error_list_train = zeros(1, num_epochs*ceil(obj.sample_size/obj.batch_size));
@@ -110,17 +117,20 @@ classdef Mind < handle
 
                     error_list_train(m*length(batches) + k - 1) = error;
                     error_list_validate(m*length(batches) + k - 1) = obj.validate;
-                    error_list_test(m*length(batches) + k - 1) = obj.test(test_data);
+                    if plot_test_error == true, error_list_test(m*length(batches) + k - 1) = obj.test(test_data); end
                 end
             end
             figure; hold on;
-            plot(error_list_train);
             box on;
             ylabel("Error");
             xlabel("Epochs");
+            plot(error_list_train);
             plot(error_list_validate, 'r');
-            plot(error_list_test, 'g');
-            legend("Training Error", "Validation Error", "Test Error");
+            legend("Training Error", "Validation Error");
+            if plot_test_error == true
+                plot(error_list_test, 'k');
+                legend("Training Error", "Validation Error", "Test Error");
+            end
             close(v);
         end
         
@@ -131,42 +141,13 @@ classdef Mind < handle
                 [length(obj.examples.validate(1).features) length(obj.examples.validate)])', false))));
         end
         
-%         function test(obj)
-%             labels = obj.scale(reshape([obj.examples.validate.labels],...
-%                 [length(obj.examples.validate(1).labels) length(obj.examples.validate)])', 'l') %#ok
-%             
-%             predictions = obj.infer(reshape([obj.examples.validate.features],...
-%                 [length(obj.examples.validate(1).features) length(obj.examples.validate)])', false) %#ok
-%             
-%             error = abs(obj.scale(reshape([obj.examples.validate.labels],...
-%                 [length(obj.examples.validate(1).labels) length(obj.examples.validate)])', 'l')...
-%                 - obj.infer(reshape([obj.examples.validate.features],...
-%                 [length(obj.examples.validate(1).features) length(obj.examples.validate)])', false)) %#ok
-%             
-%             average_error = mean(abs(obj.scale(reshape([obj.examples.validate.labels],...
-%                 [length(obj.examples.validate(1).labels) length(obj.examples.validate)])', 'l')...
-%                 - obj.infer(reshape([obj.examples.validate.features],...
-%                 [length(obj.examples.validate(1).features) length(obj.examples.validate)])', false))) %#ok
-%         end
         
         function test_error = test(obj, data)
-            labels = reshape([data.examples.labels], [length(data.examples(1).labels) length(data.examples)])';
-            
+            labels = reshape([data.examples.labels], [length(data.examples(1).labels) length(data.examples)])'; 
             predictions = obj.infer(reshape([data.examples.features],...
                 [length(data.examples(1).features) length(data.examples)])', false);
-            
             error = abs(labels - predictions);
             test_error = mean(error);
-        end
-        
-        function test_error = test2(obj, data)
-            labels = reshape([data.examples.labels], [length(data.examples(1).labels) length(data.examples)])'
-            
-            predictions = obj.infer(reshape([data.examples.features],...
-                [length(data.examples(1).features) length(data.examples)])', false)
-            
-            error = abs(labels - predictions)
-            test_error = mean(error)
         end
 
         function y = infer(obj, features, descale)
@@ -204,65 +185,6 @@ classdef Mind < handle
 
         function y = descale(obj, values)
             y = obj.label_ranges(1, :) + values.*(obj.label_ranges(2, :) - obj.label_ranges(1, :));
-        end
-
-        function test_transmission_spectrum(obj, subset, example)
-            figure; hold on;
-            x = linspace(1.4e-6, 1.7e-6, length(subset(example).labels))*1e6;
-            plot(x, -(obj.infer(subset(example).features)));
-            plot(x, -(subset(example).labels));
-            legend('Model', 'Simulation');
-            xlabel("Wavelength (µm)");
-            ylabel("Transmission (a.u.)")
-            box on
-        end
-
-        function publish(obj)
-            activation_functions = cell(1, length(obj.layers));
-            for n = 1:length(obj.layers)
-                activation_functions{n} = obj.layers(n).activation_function;
-            end
-            new_model = Model(obj.data.inputs, obj.data.outputs, obj.weights, obj.biases,...
-                activation_functions, obj.feature_ranges, obj.label_ranges);
-            save('pm-pic.mat', 'new_model');
-        end
-        
-        function copy_mind(obj, source)
-            obj.weights = source.weights;
-            obj.biases = source.biases;
-            obj.optimizer = source.optimizer;
-        end
-        
-        function prune_examples(obj)
-            m = 1;
-            while m <= length(obj.examples.train)
-                if obj.examples.train(m).labels < 0.15
-                    obj.examples.train(m) = [];
-                    m = m - 1;
-                end
-                m = m + 1;
-            end
-            
-            m = 1;
-            while m <= length(obj.examples.validate)
-                if obj.examples.validate(m).labels < 0.15
-                    obj.examples.validate(m) = [];
-                    m = m - 1;
-                end
-                m = m + 1;
-            end
-            
-            obj.sample_size = length(obj.examples.train);
-            obj.batch_size = obj.sample_size;
-        end
-        
-        function map_examples(obj)
-            features = reshape([obj.examples.train.features], [length(obj.examples.train(1).features) length(obj.examples.train)])';
-            scatter(features(:, 1)*1e9, features(:, 2), 'filled');
-            xlabel('Etch Depth (nm)');
-            ylabel('Duty Cycle (a.u.)');
-            xlim(obj.data.inputs(1).range*1e9);
-            ylim(obj.data.inputs(2).range);
         end
     end
 end
