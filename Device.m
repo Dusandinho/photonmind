41faf0e1df494c07a5f811fbd4a1701d7a1fda2d
Dusan Gostimirovic <dusan.gostimirovic@gmail.com>
2019-03-04 10:03:59 -0500

    Add files via upload

--- Device.m
+++ Device.m
@@ -12,9 +12,9 @@ classdef Device < handle
         end
 
         % constants keep certain features locked in the sweep solver
-        % these two methods add and remove constants by indexing
+        % these next two methods add and remove constants by indexing
         function add_constant(obj, index, constant)
-%             if index > length(obj.model.inputs), error('Index out of input range'); end
+            if index > length(obj.model.inputs), error('Index out of input range'); end
             obj.constants(index).value = constant;
         end
 
@@ -22,24 +22,28 @@ classdef Device < handle
             obj.constants(index).value = [];
         end
 
+        function remove_all_constants(obj)
+            for n = 1:length(obj.constants)
+                obj.constants(n) = [];
+            end
+        end
+
         % conditions are what the sweep solver looks for
-        % the use picks a value and some tolerance they will allow
-        % these two methods add and remove constants by indexing
+        % the user picks a value and some tolerance they will allow
+        % these next two methods add and remove constants by indexing
         function add_condition(obj, index, value, tolerance)
-%             if index > length(obj.model.data.outputs), error('Index out of output range'); end
+            if index > length(obj.model.outputs), error('Index out of output range'); end
             obj.conditions(index).value = value;
             obj.conditions(index).tolerance = tolerance;
         end
 
         function remove_condition(obj, index)
-            obj.conditions(index).value = [];
-            obj.conditions(index).tolerance = [];
+            obj.conditions(index) = [];
         end
-        
+
         function remove_all_conditions(obj)
-            for n = 1:4
-                obj.conditions(n).value = [];
-                obj.conditions(n).tolerance = [];
+            for n = 1:length(obj.conditions)
+                obj.conditions(n) = [];
             end
         end
 
@@ -54,20 +58,11 @@ classdef Device < handle
 
             for n = 1:length(featureset)
                 waitbar(n/length(featureset));
-%                 feats = cat(2, 1, featureset(n, :));
-                feats = featureset(n, :);
-                labels = obj.model.infer(feats);
-%                 if obj.check_conditions(labels(1:2))
+                inputs = featureset(n, :);
+                labels = obj.model.infer(inputs);
                 if obj.check_conditions(labels)
-%                     feats(1) = 0;
-%                     hld = labels(2);
-%                     labels = obj.model.infer(feats);
-%                     if obj.check_conditions2(labels(3))
-%                         if abs(hld - labels(4)) < 0.01e-6
-                            obj.matches{end + 1} = feats;
-                            obj.print_device(feats, labels);
-%                         end
-%                     end
+                    obj.matches{end + 1} = inputs;
+                    obj.print_device(inputs, labels);
                 end
             end
             close(v);
@@ -79,7 +74,7 @@ classdef Device < handle
             obj.features = cell2mat(obj.matches(match_num));
         end
 
-        % predicts the output based on the features of the device
+        % predicts the output based on the selected features of the device
         function run(obj)
             if isempty(obj.features), error('No features have been set for this device'); end
             y = obj.model.infer(obj.features);
@@ -90,12 +85,12 @@ classdef Device < handle
     end
     methods (Access = private)
         % create a uniformly distributed featureset to check
-        function y = get_uniform_featureset(obj, resolution)
-            for n = 1:length(obj.model.data.inputs)
-                sequence = linspace(obj.model.data.inputs(n).range(1),...
-                obj.model.data.inputs(n).range(2), resolution);
-                sequence = repmat(sequence, [resolution^(length(obj.model.data.inputs) - n), 1]);
-                featureset(n, :) = repmat(sequence(:), [resolution^(n - 1), 1]);
+        function featureset = get_uniform_featureset(obj, resolution)
+            for n = 1:length(obj.model.inputs)
+                sequence = linspace(obj.model.inputs(n).range(1),...
+                obj.model.inputs(n).range(2), resolution);
+                sequence = repmat(sequence, [resolution^(length(obj.model.inputs) - n), 1]);
+                featureset(n, :) = repmat(sequence(:), [resolution^(n - 1), 1]); %#ok
             end
 
             for n = 1:length(obj.constants)
@@ -103,7 +98,7 @@ classdef Device < handle
                     featureset(n, :) = obj.constants(n).value;
                 end
             end
-            y = unique(featureset', 'rows');
+            featureset = unique(featureset', 'rows');
         end
 
         % check if value is within range of conditions
@@ -111,18 +106,7 @@ classdef Device < handle
         function y = check_conditions(obj, labels)
             y = true;
             for n = 1:length(obj.conditions)
-%             for n = 1:2
                 if abs(labels(n) - obj.conditions(n).value) > abs(obj.conditions(n).tolerance)
-                    y = false;
-                end
-            end
-        end
-        
-        function y = check_conditions2(obj, labels)
-            y = true;
-%             for n = 1:length(obj.conditions)
-            for n = 3
-                if abs(labels(n - 2) - obj.conditions(n).value) > abs(obj.conditions(n).tolerance)
                     y = false;
                 end
             end
@@ -132,18 +116,12 @@ classdef Device < handle
         function print_device(obj, features, labels)
             fprintf('\n');
             disp(['Match #', num2str(length(obj.matches))]);
-%             for n = 1:length(obj.model.data.inputs)
-%                 disp([obj.model.data.inputs(n).parameter, ' = ', num2str(features(n))]);
-%             end
-%             for n = 1:length(obj.model.outputs)
-%                 disp([obj.model.outputs(n).attribute, ' = ', num2str(labels(n))]);
-%             end
-for n = 1:3
-    disp([num2str(features(n))]);
-end
-for n = 23
-    disp([num2str(labels(n))]);
-end
+            for n = 1:length(obj.model.inputs)
+                disp([obj.model.inputs(n).parameter, ' = ', num2str(features(n))]);
+            end
+            for n = 1:length(obj.model.outputs)
+                disp([obj.model.outputs(n).attribute, ' = ', num2str(labels(n))]);
+            end
         end
     end
 end
